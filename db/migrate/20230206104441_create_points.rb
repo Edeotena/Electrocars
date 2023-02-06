ce8ece2891f22c5696ffc83872f7af8187471d39
edeotena <edeotena13@gmail.com>
2023-02-06 13:44:52 +0300

    rails g model point name:string latitude:decimal longitude:decimal

---
+++ db/migrate/20230206104441_create_points.rb
@@ -0,0 +1,11 @@
+class CreatePoints < ActiveRecord::Migration[7.0]
+  def change
+    create_table :points do |t|
+      t.string :name
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
